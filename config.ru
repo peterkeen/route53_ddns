1709d6a0c125ed87afbd3cb455eddf8db54ed198
Pete Keen <peter.keen@bugsplat.info>
2015-05-06 20:26:22 -0400

    Make it so

---
+++ config.ru
@@ -0,0 +1,10 @@
+require 'updater'
+
+use Rack::Auth::Basic, 'Protected Area' do |user,pass|
+  user == ENV['USERNAME'] && pass == ENV['PASSWORD']
+end
+
+run lambda { |env|
+  SubspaceUpdater.new(Rack::Request.new(env)).update
+  return [200, {}, ['ok']]
+}
---
+++ updater.rb
@@ -0,0 +1,36 @@
+require 'aws-sdk'
+
+class SubspaceUpdater
+  def initialize(request)
+    @request = request
+  end
+
+  def update
+    conn = Aws::Route53::Client.new
+
+    opts = {
+      hosted_zone_id: ENV['ZONE_ID'],
+      change_batch: {
+        changes: [
+          {
+            action: "UPSERT",
+            resource_record_set: {
+              name: ENV['RECORD_NAME'],
+              type: "A",
+              ttl: 60,
+              resource_records: [
+                { value: @request.ip }
+              ]
+            }
+          }
+        ]
+      }
+    }
+    begin
+      resp = conn.change_resource_record_sets(opts)
+    rescue Aws::Errors::ServiceError => e
+      p e.message
+    end
+  end
+end
+
